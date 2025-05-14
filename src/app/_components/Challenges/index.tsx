451e3a50ab0f0555692a9f2c3ab0b3300e228109
duccini <guilhermeduccini@gmail.com>
2025-05-14 16:31:09 -0300

    feat: challenges numbers

--- src/app/_components/Challenges/index.tsx
+++ src/app/_components/Challenges/index.tsx
@@ -1,5 +1,56 @@
+import ChallengeCard from "@/components/ChallengCard";
 import styles from "./Challenges.module.css";
 
 export default function Challenges() {
-  return <h2>Challenge</h2>;
+  return (
+    <div className={styles.challengesContainer}>
+      <div className={styles.challengesContent}>
+        <div className={styles.challengesDetails}>
+          <ChallengeCard
+            title="4B"
+            description="Taxas geradas aos provedores de liquidez do pool usdc-weth"
+            date="Ao longo de 2024"
+            color="challengeCardGreen"
+          />
+
+          <ChallengeCard
+            title="106M"
+            description="Negociado na Uniswap"
+            date="Dezembro de 2024"
+            color="challengeCard3"
+          />
+
+          <ChallengeCard
+            title="463B"
+            description="Volume mensal negociado em DEXS"
+            date="Dezembro de 2024"
+            color="challengeCardBlue"
+          />
+
+          <ChallengeCard
+            title="96M"
+            description="Negociado na PancakeSwap"
+            date="Dezembro de 2024"
+            color="challengeCard4"
+          />
+
+          <ChallengeCard
+            title="6.26B"
+            description="Valor total alocado na uniswap v3"
+            date="Abril de 2024"
+            color="challengeCard5"
+          />
+
+          <ChallengeCard
+            title="58M"
+            description="Negociado na Raydium"
+            date="Dezembro de 2024"
+            color="challengeCard6"
+          />
+        </div>
+
+        <div className={styles.challengesHeader}></div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/ChallengCard/index.tsx
@@ -0,0 +1,23 @@
+import styles from "./ChallengeCard.module.css";
+
+interface ChallengeCard {
+  title: string;
+  description: string;
+  date: string;
+  color: string;
+}
+
+export default function ChallengeCard({
+  title,
+  description,
+  date,
+  color,
+}: ChallengeCard) {
+  return (
+    <div className={styles.challengeCard}>
+      <h3 className={`${styles.challengeTitle} ${styles[color]}`}>{title}</h3>
+      <h4 className={styles.challengeDescription}>{description}</h4>
+      <h5 className={styles.challengeDate}>{date}</h5>
+    </div>
+  );
+}
