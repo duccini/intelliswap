941cb0f385b2322fad4f367db0a19132ce95761f
duccini <guilhermeduccini@gmail.com>
2025-05-17 20:22:49 -0300

    refactor folders structure

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -36,9 +36,9 @@ export default function Footer() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentcolor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               className="lucide lucide-x-twitter"
             >
               {" "}
