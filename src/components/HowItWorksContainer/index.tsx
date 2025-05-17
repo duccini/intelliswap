367226899bd37b711fefa0c8b9c68e78ed18ed59
duccini <guilhermeduccini@gmail.com>
2025-05-17 17:26:16 -0300

    feat: update how it works content

--- src/components/HowItWorksContainer/index.tsx
+++ src/components/HowItWorksContainer/index.tsx
@@ -8,32 +8,33 @@ const cards = {
   riskProfile: {
     title: "Perfil de risco",
     description:
-      "Através de um questionário, a IA analisa o seu perfil de risco e sugere o melhor investimento para você.",
-    image: "/image1.jpg",
+      "Selecionar os pools de liquidez baseado no seu perfil de risco no IntelliSwap é literalmente simples como clicar em um botão.\nNo Risco Baixo você verá pools onde os 2 tokens são  stablecoins. No  Risco Médio você verá tokens com 1 stablecoin e 1 altcoin. Risco Alto você verá pools onde os 2 tokens são altcoins.",
+    image: "/intelliswap_table.jpg",
+  },
+  zkVerify: {
+    title: "ZKVerify",
+    description:
+      "IntelliSwap utiliza a tecnologia ZKVerify da empresa americana Horizon Labs para garantir a segurança e transparência dos contratos inteligentes e dos modelos de IA.\nDessa forma, você pode ficar tranquilo sabendo que o seu investimento está seguro e verificado.",
+    image: "/zkverify.jpg",
   },
   verifiedAI: {
     title: "IA Verificada",
     description:
-      "A IA é verificada por especialistas do mercado financeiro, garantindo segurança e confiabilidade.",
-    image: "/image2.jpg",
+      "IA caixa preta? Não no IntelliSwap! Utilizamos modelos e bases de dados auditados pelo tecnologia ZKVerify da empresa americana Horizon Labs.\nDessa forma, oferecemos o que existe de melhor no mercado para precificação de preço reforçando nossa transparência e confiabilidade.",
+    image: "/verified-ia.png",
   },
-  liquidityPool: {
-    title: "Pool de Liquidez",
-    description:
-      "A liquidez é garantida através de um pool de liquidez, permitindo que você compre e venda seus ativos a qualquer momento.",
-    image: "/image3.jpg",
-  },
+
   totalValue: {
     title: "Valor total",
     description:
-      "O valor total investido é atualizado em tempo real, permitindo que você acompanhe o desempenho do seu investimento.",
-    image: "/image4.jpg",
+      "No IntelliSwap você não precisa adquirir os tokens do pools de liquidez nem calcular a distribuição de cada token na pool. \nO IntelliSwap faz tudo isso para você!",
+    image: "/total-value.png",
   },
   invest: {
     title: "Investir",
     description:
-      "O valor total investido é atualizado em tempo real, permitindo que você acompanhe o desempenho do seu investimento.",
-    image: "/image5.jpg",
+      "O IntelliSwap guarda o seu investimento em contratos inteligentes verificados pela tecnologia ZKVerify.\nDessa forma, você tem a segurança que o seu investimento está sendo feito exatamente como combinado, sem surpresas quando você for liquidar a sua posição.",
+    image: "/risk-profile.jpg",
   },
 };
 
@@ -47,11 +48,9 @@ export default function HowItWorksContainer() {
           <li onClick={() => setCardItem(cards.riskProfile)}>
             Perfil de risco
           </li>
-
+          <li onClick={() => setCardItem(cards.zkVerify)}>ZK Verify</li>
           <li onClick={() => setCardItem(cards.verifiedAI)}>IA Verificada</li>
-          <li onClick={() => setCardItem(cards.liquidityPool)}>
-            Pool de Liquidez
-          </li>
+
           <li onClick={() => setCardItem(cards.totalValue)}>Valor total</li>
           <li onClick={() => setCardItem(cards.invest)}>Investir</li>
         </ul>
